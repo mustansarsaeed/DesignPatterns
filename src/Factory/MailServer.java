5467ce614d7ed7703267580b837c26240e6ac1d2
Mustansar Saeed <m.mustansar.saeed@gmail.com>
2022-03-30 21:23:53 +0500

    Update MailServer.java

--- src/Factory/MailServer.java
+++ src/Factory/MailServer.java
@@ -1,6 +1,9 @@
 package Factory;
 
 public class MailServer implements IServer {
+    public void sendDataToServer(String url) {
+        System.out.println("Sending the data."); // for the testing purpose
+    }
     @Override
     public void displayServer() {
         System.out.println("Mail Server");
